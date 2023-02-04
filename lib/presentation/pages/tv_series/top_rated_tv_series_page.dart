9eeeccde36479773d16c4dd34975b11ae6460ac7
Heri Sarwanto <heri.sarwanto@majoo.id>
2023-02-04 11:46:07 +0700

    Remove unused import

--- lib/presentation/pages/tv_series/top_rated_tv_series_page.dart
+++ lib/presentation/pages/tv_series/top_rated_tv_series_page.dart
@@ -1,7 +1,5 @@
 import 'package:ditonton/common/state_enum.dart';
-import 'package:ditonton/presentation/provider/movies/top_rated_movies_notifier.dart';
 import 'package:ditonton/presentation/provider/tv_series/top_rated_tv_series_notifier.dart';
-import 'package:ditonton/presentation/widgets/movie_card_list.dart';
 import 'package:ditonton/presentation/widgets/tv_series_card_list.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
